082106ad6249f03f8a4eac9063e41e96a9a35e80
mayanksankhla3007 <164235151+mayanksankhla3007@users.noreply.github.com>
2025-07-19 17:21:51 +0530

    Update train_gripper_model.py

--- train_gripper_model.py
+++ train_gripper_model.py
@@ -3,12 +3,12 @@ from ultralytics import YOLO
 model = YOLO('yolov8n.pt')  # or yolov8s.pt for a slightly larger model
 
 model.train(
-    data='/media/ihub/9CFC-8D2F/Project/gripper_dataset/dataset.yaml',
+    data='gripper_dataset/dataset.yaml',
     epochs=10,
     imgsz=640,
     batch=16,
     device='cpu',  # Use 0 for GPU, 'cpu' for CPU
     project='gripper_detection',
-    name='yolov8_gripper_model'
+    name='gripper_detection_model'
 )
-print("✅ Training complete. The model is saved in the 'gripper_detection/yolov8_gripper_model' directory.")
+print("✅ Training complete. The model is saved in the 'gripper_detection/gripper_detection_model' directory.")
